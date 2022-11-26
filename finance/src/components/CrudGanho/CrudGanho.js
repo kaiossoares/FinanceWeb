76984e71c10f294fb48c2dc71f3b16adca530afb
Kaio Soares <kaioantoniosoares@gmail.com>
2022-11-26 17:32:12 -0300

    Fix FrontEnd

--- finance/src/components/CrudGanho/CrudGanho.js
+++ finance/src/components/CrudGanho/CrudGanho.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import './CrudGanho.css'
 import Main from '../template/Main'
+import editButton from '../../assets/images/editButton.svg'
+import deleteButton from '../../assets/images/deleteButton.svg'
 
 const urlAPI = "http://localhost:5165/api/ganho";
-const title = "Ganhos a cada mês";
 
 const initialState = {
     ganho: { id: 0, nomeGanho: '', valorGanho: 0 },
@@ -21,34 +22,129 @@ export default class CrudGanho extends Component {
         })
     }
 
+    limpar() {
+        this.setState({ ganho: initialState.ganho })
+    }
+
+    salvar() {
+        const ganho = this.state.ganho
+        const metodo = ganho.id ? 'put' : 'post'
+        const url = ganho.id ? `${urlAPI}/${ganho.id}` : urlAPI
+
+        axios[metodo](url, ganho)
+            .then(resp => {
+                const lista = this.getListaAtualizada(resp.data)
+                this.setState({ ganho: initialState.ganho, lista })
+            })
+    }
+
+    getListaAtualizada(ganho, add = true) {
+        const lista = this.state.lista.filter(g => g.id !== ganho.id)
+        if (add) lista.unshift(ganho)
+        return lista
+    }
+
+    atualizaCampo(event) {
+        const ganho = { ...this.state.ganho }
+        ganho[event.target.name] = event.target.value
+        this.setState({ ganho })
+    }
+
+    carregar(ganho) {
+        this.setState({ ganho })
+    }
+
+    remover(ganho) {
+        const url = urlAPI + "/" + ganho.id;
+        if (window.confirm("Confirma remoção do ativo: " + ganho.nomeGanho)) {
+            console.log("entrou no confirm");
+
+            axios['delete'](url, ganho)
+                .then(resp => {
+                    const lista = this.getListaAtualizada(ganho, false)
+                    this.setState({ ganho: initialState.ganho, lista })
+                })
+        }
+    }
+
     renderTable() {
         return (
-            <div className="listagem">
-                <table className="listaGanhos" id="tblListaGanhos">
-                    <thead>
-                        <tr className="cabecTabela">
-                            <th className="tabTituloNome">Nome Ganho</th>
-                            <th className="tabTituloCurso">Valor Ganho</th>
-                        </tr>
-                    </thead>
 
-                    <tbody>
-                        {this.state.lista.map(
-                            (ganho) =>
-                                <tr key={ganho.id}>
-                                    <td>{ganho.nomeGanho}</td>
-                                    <td>{ganho.valorGanho}</td>
-                                </tr>
-                        )}
-                    </tbody>
-                </table>
+            <div className="financeCard">
+                <div className="inclui-container">
+                    <label> Nome do Ativo: </label>
+                    <input
+                        type="text"
+                        id="nomeGanho"
+                        placeholder="Nome do Ativo"
+                        className="form-input"
+                        name="nomeGanho"
+
+                        value={this.state.ganho.nomeGanho}
+
+                        onChange={e => this.atualizaCampo(e)}
+                    />
+
+                    <label> Valor do Ativo: </label>
+                    <input
+                        type="text"
+                        id="valorGanho"
+                        placeholder="Valor do Ganho"
+                        className="form-input"
+                        name="valorGanho"
+
+                        value={this.state.ganho.valorGanho}
+
+                        onChange={e => this.atualizaCampo(e)}
+                    />
+
+                    <button className="btn"
+                        onClick={e => this.salvar(e)} >
+                        Salvar
+                    </button>
+                    <button className="btn"
+                        onClick={e => this.limpar(e)} >
+                        Cancelar
+                    </button>
+                </div>
+
+                <div className="listagem">
+                    <table className="listaGanhos" id="tblListaGanhos">
+                        <thead>
+                            <tr className="cabecTabela">
+                                <th className="tabTituloNome" id='tab'>Nome do Ativo</th>
+                                <th className="tabTituloValor" id='tab'>Valor do Ativo</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            {this.state.lista.map(
+                                (ganho) =>
+                                    <tr key={ganho.id}>
+                                        <td id='nomeGanho'>{ganho.nomeGanho}</td>
+                                        <td id='valorGanho'>R$ {ganho.valorGanho.toFixed(2)}</td>
+                                        <td id='editButton'>
+                                            <div className="edit-button" onClick={() => this.carregar(ganho)}>
+                                                <img src={editButton} alt="Editar" />
+                                            </div>
+                                        </td>
+                                        <td id='deleteButton'>
+                                            <div className="delete-button" onClick={() => this.remover(ganho)}>
+                                                <img src={deleteButton} alt="Deletar" />
+                                            </div>
+                                        </td>
+                                    </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         )
     }
 
     render() {
         return (
-            <Main title={title}>
+            <Main>
                 {this.renderTable()}
             </Main>
         )
